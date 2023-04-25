684def74a89a3dba27f4f8d147f38fe784d6f152
Claudio Daniel Pacheco Castro <claudio@comunidad.unam.mx>
2023-04-25 02:13:23 -0600

    Update dashboard_rezago.py

--- dashboard/dashboard_rezago.py
+++ dashboard/dashboard_rezago.py
@@ -2,42 +2,46 @@ import pandas as pd
 import streamlit as st
 import plotly.graph_objs as go
 
+
 st.set_page_config(page_title="Estimación del rezago habitacional utilizando imágenes satelitales", page_icon=":house:")
 
-st.title("Estimación del rezago habitacional utilizando imágenes satelitales")
+
+
+
+st.markdown("<p style='font-family: Montserrat; font-weight: bold;font-size: 35px; text-align: center'>Estimación del rezago habitacional utilizando imágenes satelitales</p>", unsafe_allow_html=True)
 st.image("https://centrourbano.com/revista/wp-content/uploads/Dia-Nacional-de-la-Vivienda-se-reduce-el-rezago-habitacional-en-Mexico-1280x720.jpg", width=700)
-st.header("¿Qué es el rezago habitacional?")
+st.markdown("<p style='font-family: Montserrat; font-weight: bold;font-size: 20px; text-align: center'>¿Qué es el rezago habitacional?</p>", unsafe_allow_html=True)
+
 #Añadir mapa de folium
 
 st.write("El rezago habitacional es la diferencia entre la vivienda que se necesita y la que se tiene. Se mide en términos de la calidad de la vivienda, el acceso a servicios básicos y la seguridad de la vivienda. El rezago habitacional se mide en términos de la calidad de la vivienda, el acceso a servicios básicos y la seguridad de la vivienda. El rezago habitacional se mide en términos de la calidad de la vivienda, el acceso a servicios básicos y la seguridad de la vivienda. El rezago habitacional se mide en términos de la calidad de la vivienda, el acceso a servicios básicos y la seguridad de la vivienda. El rezago habitacional se mide en términos de la calidad de la vivienda, el acceso a servicios básicos y la seguridad de la vivienda. El rezago habitacional se mide en términos de la calidad de la vivienda, el acceso a servicios básicos y la seguridad de la vivienda. El rezago habitacional se mide en términos de la calidad de la ")
 
-st.header("¿Cómo se mide el rezago habitacional?")
+st.markdown("<p style='font-family: Montserrat; font-weight: bold;font-size: 20px; text-align: center'>¿Cómo se mide el rezago habitacional?</p>", unsafe_allow_html=True)
 st.image("https://github.com/claudiodanielpc/proyecto_infotec/raw/main/rezago.png", width=700)
 
 
 #Añadir sidebar
-st.sidebar.title("Menú")
-st.sidebar.header("Información relevante")
-st.sidebar.write("¿Quieres saber más?")
+st.sidebar.markdown("<p style='font-family: Montserrat; font-weight: bold;'>Menú</p>", unsafe_allow_html=True)
+st.sidebar.markdown("<p style='font-family: Montserrat;'>¿Quiéres saber más?</p>", unsafe_allow_html=True)
 
 #Añadir opciones
 option = st.sidebar.selectbox(
-    'Opciones',
-        ['Sobre el proyecto', 'Fuentes de información'])
+    'Selecciona una opción',
+        ['Sobre el proyecto', 'Fuentes de información']) #Formato de la fuente   
 
 if option == 'Sobre el proyecto':
     st.sidebar.write("El proyecto tiene como objetivo proporcionar una forma alternativa de medición del rezago habitacional utilizando imágenes satelitales.")
     st.sidebar.write("La idea no solo es xxx")
 
 if option== 'Fuentes de información':
-    st.sidebar.write("Las fuentes de información utilizadas para este proyecto son:")
+    st.sidebar.write("<p style='font-family: Montserrat;'>Las fuentes de información utilizadas para este proyecto son:</p>", unsafe_allow_html=True)
     #ENIGH
     url = "https://www.inegi.org.mx/img/programas/enchogares/ENIGH_ch.gif"
     caption = "INEGI. Encuesta Nacional de Ingresos y Gastos de los Hogares"
 
 
     st.sidebar.markdown(
-    f"<div style='text-align:center;'>"
+    f"<div style='text-align:center;font-family:montserrat;'>"
     f"<img src='{url}' alt='{caption}' width='70'/>"
     #Añadir url para redirigir a la página del INEGI
     f"<p><a href='https://www.inegi.org.mx/programas/enigh/nc/2020/'>INEGI. Encuesta Nacional de Ingresos y Gastos de los Hogares</a></p>"
@@ -52,7 +56,7 @@ if option== 'Fuentes de información':
     caption = "INEGI. Censo de Población y Vivienda 2020"
 #Censo
     st.sidebar.markdown(
-    f"<div style='text-align:center;'>"
+    f"<div style='text-align:center; font-family:montserrat;'>"
     f"<img src='{url}' alt='{caption}' width='70'/>"
     #Añadir url para redirigir a la página del INEGI
     f"<p><a href='https://www.inegi.org.mx/programas/ccpv/2020/'>INEGI. Censo de Población y Vivienda 2020</a></p>"
@@ -63,13 +67,15 @@ if option== 'Fuentes de información':
     url="https://earthengine.google.com/static/images/GoogleEarthEngine_Grey_108.png"
     caption="Google Earth Engine"
     st.sidebar.markdown(
-    f"<div style='text-align:center;'>"
+    f"<div style='text-align:center; font-family:montserrat;'>"
     f"<img src='{url}' alt='{caption}' width='70'/>"
     #Añadir url para redirigir a la página del INEGI
     f"<p><a href='https://earthengine.google.com/'>Google Earth Engine</a></p>"
     f"</div>",
     unsafe_allow_html=True)
-    
+
+
+
 
 
 #Mapa
@@ -105,3 +111,18 @@ layout = go.Layout(
 fig = go.Figure(data=data, layout=layout)
 
 st.plotly_chart(fig)
+
+st.markdown("---")
+
+left_info_col, right_info_col = st.columns(2)
+
+left_info_col.markdown(
+        f"""
+        ### Autor
+        Comentarios, preguntas o sugerencias.
+        ##### Claudio Daniel Pacheco-Castro [![Twitter URL](https://img.shields.io/twitter/url/https/twitter.com/bukotsunikki.svg?style=social&label=Follow%20%40claudiodanielpc)](https://twitter.com/claudiodanielpc)
+        - Email:  <claudio@comunidad.unam.mx> o <claudiodanielpc@gmail.com>
+        - GitHub: https://github.com/claudiodanielpc
+        """,
+        unsafe_allow_html=True,
+    )
